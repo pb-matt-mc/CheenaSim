a0ab36c63aa5f3309bd5cd46a5ae878e1dd3ec1c
pb-matt-mc <pbmatt.mc@gmail.com>
2026-06-24 02:36:33 -0400

    auto: checkpoint 02:36

--- digital-world/src/main.ts
+++ digital-world/src/main.ts
@@ -1,13 +1,14 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene';
 import { HomeScene } from './scenes/HomeScene';
+import { PreloadScene } from './scenes/PreloadScene';
 
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 960,
   height: 640,
   backgroundColor: '#1a1a2e',
-  scene: [BootScene, HomeScene],
+  scene: [BootScene, PreloadScene, HomeScene],
   parent: document.body,
   scale: {
     mode: Phaser.Scale.FIT,
